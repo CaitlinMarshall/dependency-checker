a68cb97e7fa3abc28a7eca4b23e094e6ae8b0cbc
Caitlin Marshall <caitlin.marshall@gmail.com>
2021-05-13 15:07:46 -0400

    datatable working

--- force-app/main/default/lwc/metadataCheck/metadataCheck.js
+++ force-app/main/default/lwc/metadataCheck/metadataCheck.js
@@ -1,27 +1,32 @@
-import { LightningElement, wire } from 'lwc';
+import { LightningElement, track } from 'lwc';
 import getDependencies from '@salesforce/apex/DependencyController.getDependencies';
 
 const columns = [
-    { label: 'Component Type', fieldName: 'MetadataComponentType', type: 'text' },
-    { label: 'Component Name', fieldName: 'MetadataComponentName', type: 'text' },
     { label: 'Component Id', fieldName: 'MetadataComponentId', type: 'text' },
+    { label: 'Component Name', fieldName: 'MetadataComponentName', type: 'text' },
+    { label: 'Component Type', fieldName: 'MetadataComponentType', type: 'text' },
     { label: 'Dependency Name', fieldName: 'RefMetadataComponentName', type: 'text' },
     { label: 'Dependency Type', fieldName: 'RefMetadataComponentType', type: 'text' },
 
 ];
 
 export default class MetadataCheck extends LightningElement {
-    dependencies;
-    error;
+    @track data;
+    @track error;
+    columns = columns;
 
 
     handleGetDependencies() {
+        alert("Button clicked!");
         getDependencies()
             .then(result => {
-                this.dependencies = result;
+                this.data = JSON.parse(result).records;
+                console.log(this.data);
+
             })
             .catch(error => {
                 this.error = error;
+                console.log(this.error);
             })
     }
 
